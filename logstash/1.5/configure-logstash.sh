dd5fbb62b40d3f5d284a0489498718894ca7b3ba
Julien Cruz <julien.cruz@gmail.com>
2015-06-26 01:31:28 -0400

    Reverted modifications to allow logstash/conf.d to be specified as a file.

--- logstash/1.5/configure-logstash.sh
+++ logstash/1.5/configure-logstash.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 # Copy scripts from the build to the runtime directory
-if [ -d "/etc/logstash/conf.d" ] ; then
-    cp -rf /etc/logstash/conf.d /etc/logstash/run.d
-else
-    mkdir -p /etc/logstash/run.d
-    cp /etc/logstash/conf.d /etc/logstash/run.d/logstash.conf
-fi
+cp -rf /etc/logstash/conf.d /etc/logstash/run.d
